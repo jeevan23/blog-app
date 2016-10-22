03a30cc5119c21117861a39c90be51053f8727e4
jeevan23 <guptajeevandeep@gmail.com>
2016-10-22 22:53:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element=document.getElementById("new");
 element.innerHTML="new value";
+
+var img=document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginLeft="100 px"
+}
