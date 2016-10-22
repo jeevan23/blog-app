a33d14a1293cb9378cf83f3c718fa54fe98501a5
jeevan23 <guptajeevandeep@gmail.com>
2016-10-22 23:06:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,9 @@ var element=document.getElementById("new");
 element.innerHTML="new value";
 
 var img=document.getElementById("madi");
+
+var marginLeft=0;
+
 function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+"px";
