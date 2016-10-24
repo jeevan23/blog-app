1ffbeaef66d0085d7275603f25a3af6142384223
jeevan23 <guptajeevandeep@gmail.com>
2016-10-24 11:49:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,16 +88,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function(req,res){
+   counter=counter+1;
+   req.send(counter.tostring());
+});
+
 app.get('/:articleName',function(req, res) {
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
 
-var counter=0;
-app.get('/counter', function(req,res){
-   counter=counter+1;
-   req.send(counter.tostring())
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
