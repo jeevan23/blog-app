61d52a2a49d2c81878dc08762e428308906cb7a3
jeevan23 <guptajeevandeep@gmail.com>
2016-10-24 19:08:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,16 +12,15 @@ img.onclick=function(){
 };
 
 var button=document.getElementById("counter");
-button.onclick=function(){
+button.onclick=function (){
 
 var request=new XMLHttpRequest();
-request.onreadystatechange=function()
-{
+request.onreadystatechange=function (){
     if(request.readyState===XMLHttpRequest.DONE){
         if(request.status===200){
             var counter=request.responseText;
             var span=document.getElementById("count");
-            span.innerHTML=count.toString();
+            span.innerHTML=counter.toString();
         }
     }
 };
