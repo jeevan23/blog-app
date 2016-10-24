8d8eebe0ef07c70bd876eb9482d3ed47bfe47718
jeevan23 <guptajeevandeep@gmail.com>
2016-10-24 14:33:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,14 @@ function moveRight(){
 }
 
 img.onclick=function(){
-    var interval=setInterval(moveRight,40)
+    var interval=setInterval(moveRight,40);
+};
+
+var button=document.getElementById("counter");
+var counter=0;
+button.onclick=function(){
+    
+    counter=counter+1;
+    document.getElementById("count").innerHTML=counter;
+    
 };
