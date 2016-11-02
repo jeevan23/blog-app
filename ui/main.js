36c270c48ec0b2791b49bd14a120fe943fa2603e
jeevan23 <guptajeevandeep@gmail.com>
2016-11-03 01:01:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,2 @@
 var   button=document.getElementById("submit");
-button.onclick=function(){
-    "javascript:location.href='http://jeevan23.imad.hasura-app.io/test-db'";
-};
+button.onclick="javascript:location.href='http://jeevan23.imad.hasura-app.io/test-db'";
