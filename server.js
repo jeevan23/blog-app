4b84b18074655d660836953154a1c24f61e57462
jeevan23 <guptajeevandeep@gmail.com>
2016-11-03 11:04:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-
-var config = {
-  host: 'db.imad.hasura-app.io',
-  port: '5432',
-  user: 'jeevan23',
-  password: 'process.env.DB_PASSWORD',
-  database: 'jeevan23',
-};
 
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    
 
+'articleone':
+{
+  title:'Article-one|Jeevan Deep',
+  heading:'Article One',
+  date:'oct 21,2016',
+  content:`
+                <p>
+                    This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+                </p>
+                <p>
+                    This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+                </p>
+                <p>
+                    This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+                </p>`
+  
+},
+'articletwo':
+{
+   title:'Article-two|Jeevan Deep',
+  heading:'Article Two',
+  date:'oct 25,2016',
+  content:`
+                <p>
+                    This is the content of my second article.
+                </p>`
+},
+'article-three':
+{
+   title:'Article-three|Jeevan Deep',
+  heading:'Article Three',
+  date:'oct 28,2016',
+  content:`
+                <p>
+                    This is the content of my third article.
+                </p>`
+}
+
+};
 function createTemplate(data)
 {
     var title=data.title;
@@ -52,22 +84,17 @@ function createTemplate(data)
     return htmlTemplate;
 }
 
-app.get('/Leaderboard.html',function(req, res){
-    res.sendFile(path.join(__dirname,'Leaderboard.html'));
-});
-
-
-
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
 
 app.get('/:articleName',function(req, res) {
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -82,3 +109,4 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
