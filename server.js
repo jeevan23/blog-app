94b1f6daa6181b35f850a35e53db63ba531aafed
jeevan23 <guptajeevandeep@gmail.com>
2016-11-05 18:03:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
-
-var app = express();
 var Pool = require('pg').Pool;
-app.use(morgan('combined'));
-
-
 
 var config = {
                  user: 'jeevan23',
                  database: 'jeevan23',
                   host: 'db.imad.hasura-app.io',
                   port: '5432',
-                  password: process.env.DB_PASSWORD,
-                  DATABASE_URL: process.env.DATABASE_URL
+                  password: process.env.DB_PASSWORD
 };
+
+var app = express();
+
+app.use(morgan('combined'));
+
+
+
+
 
 var articles={
     
@@ -102,7 +103,7 @@ app.get('/', function (req, res) {
 });
 
 
-var pool=new Pool('config');
+var pool=new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query('SELECT * FROM test ',function(err,result){
        if (err){
