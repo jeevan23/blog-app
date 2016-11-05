79073c7cc67141f2893e66fc5b15279412c227ac
jeevan23 <guptajeevandeep@gmail.com>
2016-11-05 19:15:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/test-db',function(req,res){
        if (err){
        res.status(500).send(err.toString());
    } else{
-       res.send(result.rows);
+       res.send(JSON.stringify(result.rows));
    }
    
    });
