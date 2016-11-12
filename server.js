7d68a855d6adccf881d745245f437592d577c85a
jeevan23 <guptajeevandeep@gmail.com>
2016-11-12 23:56:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,8 +64,15 @@ function createTemplate(data)
               <br><br>
             ${data.content}
             <br><br>
-             
-                  
+             <h4>Comments</h4>
+              <div id="comment_form">
+              
+              </div>
+              <div id="comments">
+              
+                <center>Loading comments...</center>
+                
+              </div>
         </div>
         
         <div class="footer">
@@ -82,7 +89,7 @@ function createTemplate(data)
         
         
         
-        </script>
+        <script type="text/javascript" src="/ui/Article.js"></script>
         
         <!-- Go to www.addthis.com/dashboard to customize your tools --> 
         <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-58181de50e8b802c"></script> 
