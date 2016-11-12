125344fe95b9befb110cf987a7802f243ff3459c
jeevan23 <guptajeevandeep@gmail.com>
2016-11-12 20:18:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,7 @@ function loadContents(){
           
           var bodyContent=document.getElementById("includedContent");
           bodyContent.innerHTML=str;
+          link(obj);
           
       }
       }
@@ -34,8 +35,30 @@ function loadContents(){
   request.send(); 
 }
 
-function link(contents){
-    
+function link(obj1){
+    for(var i=0;i<obj1.length;i++){
+        obj1[i].heading.onclick=function(){
+          var request1=new XMLHttpRequest();
+          request1.onreadystatechange=function(){
+              if(request.readyState==XMLHttpRequest.DONE){
+                  if(request.status==200){
+                      var content1=JSON.parse(request1.responseText);
+                      var str='<ul>';
+                      str+=`<li>
+                                    <span id="link_header">${content1[i].heading}</span>
+                                    <br><br>
+                                    <img src="/user.png"/> ${content1[i].author} <img src="/cal.png"/> ${content1[i].date}
+                                    <br><br>
+                                    ${content1[i].content}
+                                    <br><br>
+                            </li>`;
+                            
+                        str+='</ul>';
+                  }
+              }
+          };
+        };
+    }
 }
 
 loadContents();
